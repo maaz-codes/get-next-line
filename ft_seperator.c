ff3b56f99d30e6da26ddf1718279ea15553e4bbd
maaz-codes <maazkhan7454@gmail.com>
2024-05-06 19:54:41 +0530

    cleaned up files

--- ft_seperator.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
-{
-    char *str = "123\n";
-    
-}
--- get_next_line.c
+++ get_next_line.c
@@ -5,22 +5,38 @@
 #include <string.h>
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 15
+#define BUFFER_SIZE 10
 
-int find_next_line(char *buff, int count, int chars_read)
+char *fetch_line(char *buff, int chars_read)
+{
+    int i;
+    char *line;
+
+    line = malloc(sizeof(char) * ft_strlen(buff) + 1);
+    if (!line)
+        return (NULL);
+    i = 0;
+    while (buff[i] != '\n' && buff[i] != '\0')
+    {
+        line[i] = buff[i];
+        i++;
+    }
+    line[i] = '\0'; 
+    return (line);
+}
+
+int find_next_line2(char *buff, int chars_read)
 {
     int i;
 
     i = 0;
-    while (i < count)
+    if (chars_read == 0)
+        return (1);
+    while (buff[i] != '\0')
     {
         if (buff[i] == '\n')
             return (1);
         i++;
-    }
-    if (chars_read == 0)
-    {
-        return (1);
     }
     return (0);
 }
@@ -28,77 +44,45 @@ int find_next_line(char *buff, int count, int chars_read)
 char *get_next_line(int fd)
 {
     static char buff[BUFFER_SIZE + 1u];
-    int count;
-    int chars_read;
-    char **temp_split;
     char *line;
-    int i = 1;
+    int chars_read;
+    int count;
 
-    // printf("Original Buff = %s\n", buff);
+    chars_read = -1;
     count = ft_strlen(buff);
-    chars_read = 0; 
-    // printf("count = %d\n", count);
-
-    while (chars_read != -1)
+    while (chars_read != 0)
     {
-        printf("Buff = %s\n", buff);
-        if (find_next_line(buff, count, chars_read))
+        if(find_next_line2(buff, chars_read))
         {
-            if (chars_read == 0) // EOF
-                return (buff);
-            line = ft_strchr(buff, '\n');
-            printf("line = %s\n", line);
-            temp_split = ft_split(buff, '\n');
-            // printf("OLD Buff = %s\n", buff);
-            printf("word_count = %d\n", ft_word_count(buff, '\n'));
-            if (ft_word_count(buff, '\n') <= 1)
-            {
-                strcpy(buff, "\0");
-            }
-            else
-                strcpy(buff, line);
-            // memmove(buff, temp_split[1], ft_strlen(temp_split[1]));
-            // free(temp);
-            // free(temp_split);
-            // printf("NEW Buff = %s\n", buff);
-            break;
+            line = fetch_line(buff, chars_read);
+            memcpy(buff, ft_strchr(buff, '\n'), strlen(ft_strchr(buff, '\n')) + 1);
+            return (line);
         }
         chars_read = read(fd, buff + count, BUFFER_SIZE);
         count += chars_read;
         buff[count] = '\0';
     }
-    // printf("Charsread = %d\n", chars_read);
-    return (temp_split[0]);
+    return (line);
 }
 
 int main(void)
 {
+    char **lines;
+    int i;
     int fd;
-    char *line1;
-    char *line2;
-    char *line3;
-    char *line4;
-    char *line5;
 
+    i = 0;
     fd = open("read.txt", O_RDONLY);
-
-    line1 = get_next_line(fd);
-    printf("Line1 = %s\n", line1);
-    printf("\n");
-
-    line2 = get_next_line(fd);
-    printf("Line2 = %s\n", line2);
-    printf("\n");
-
-    line3 = get_next_line(fd);
-    printf("Line3 = %s\n", line3);
-    printf("\n");
-
-    line4 = get_next_line(fd);
-    printf("Line4 = %s\n", line4);
-    printf("\n");
-
-    line5 = get_next_line(fd);
-    printf("Line5 = %s\n", line5);
-    printf("\n");
+    printf("Line 1 = %s \n", get_next_line(fd));
+    printf("Line 2 = %s \n", get_next_line(fd));
+    printf("Line 3 = %s \n", get_next_line(fd));
+    printf("Line 4 = %s \n", get_next_line(fd));
+    printf("Line 5 = %s \n", get_next_line(fd));
+    printf("Line 6 = %s \n", get_next_line(fd));
+    // while (i < 1)
+    // {
+    //     lines[i] = get_next_line(fd);
+    //     printf("Line %d = %s\n", (i + 1), lines[i]);
+    //     i++;
+    // }
 }
